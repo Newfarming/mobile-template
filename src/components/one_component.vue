9c9fe1ae02a308a39c68e3e49e3514a30595bbb0
Newfarming <Newfarming@NewfarmingdeMacBook-Pro.local>
2018-10-25 15:16:39 +0800

    1

---
+++ src/components/one_component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>一个组件</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      addressForm: {
+        type: Array,
+        default: () => []
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <div class="home" id="home">
+    12345
+</template>
+
+<script>
+  // import ProductList from '@/components/ProductList'
+  export default {
+    components: {
+    },
+    async created() {
+      // this.$loading.show()
+    },
+
+    data() {
+      return {
+        isShowGenderChoose: false,
+      }
+    },
+
+    methods: {
+    },
+
+    computed: {
+      // ...mapState({
+      //   countryCode: 'countryCode'
+      // }),
+
+      // ...mapGetters({
+      //   amountOff: 'amountOff'
+      // })
+
+
+    },
+
+    mounted() {
+    },
+
+    activated() {
+    },
+
+    deactivated() {
+    },
+
+    beforeRouteLeave(to, from, next) {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .banner-container {
+    width: 100%;
+  }
+  .activity {
+    width: 100%;
+    img {
+      max-width: 100%;
+      display: block;
+    }
+  }
+  #download-observer {
+    width: 100%;
+    height: 0;
+  }
+  .block {
+    height: px2rem(40);
+  }
+</style>
